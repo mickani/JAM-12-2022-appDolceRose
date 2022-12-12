a360d59c5222339be8b2771d1d3834d3789b499f
Micaela <mickaelabossio@hotmail.com>
2022-12-12 20:55:50 -0300

    front:'listado-compras', front+back:'listado_stock_critico'

--- listado_compras.php
+++ listado_compras.php
@@ -1,9 +1,9 @@
 <?php include("db.php") ?>
 <?php include("header.php") ?>
 
-<hr class="border border-success">
-<h2 align="center" class="text-success p-4">LISTA DE COMPRAS</h2>
-<hr class="border border-success">
+<hr class="border border-primary">
+<h2 align="center" class="text-primary p-4">LISTA DE COMPRAS</h2>
+<hr class="border border-primary">
 
 <div class="container text-center">
     <div class="row align-items-start border rounded p-3">
@@ -17,30 +17,74 @@
         </div>
         <div class="row align-items-center m-2">
             <div class="col">
-                <button style="width:20%" class="btn btn-success"><b>Buscar</b></button>
+                <button style="width:20%" class="btn btn-primary"><b>Buscar</b></button>
             </div>
+        </div>
+
+        <div class="mt-5">
+            <h4 class="text-secondary">Listado de compras por fechas</h4>
+            <table class="table table-striped bg-secondary rounded" style="--bs-bg-opacity: .2;">
+                <thead>
+                    <tr>
+                        <th><b>id_compra</b></th>
+                        <th><b>id_proveedor (nombre)</b></th>
+                        <th><b>Fecha</b></th>
+                        <th><b>Detalles</b></th>
+                    </tr>
+                </thead>
+
+                <tbody>
+
+                    <!-- PHP -->
+                    <?php
+                            $query = "SELECT * FROM compras ORDER BY id_compra";
+                            $result = mysqli_query($conn, $query);    
+                            while($row = mysqli_fetch_assoc($result)) { ?>
+
+                    <tr>
+                        <td>
+                            <p class="text-dark"><b><?php echo $row['id_compra']; ?></b></p>
+                        </td>
+                        <td>
+                            <p class="text-dark"><?php echo $row['id_proveedor']; ?>
+                        </td>
+                        <td>
+                            <p class="text-dark"><?php echo $row['fecha_compra']; ?></p>
+                        </td>
+                        <td>
+                            <button type="button" class="btn btn-secondary">Ver</button>
+                        </td>
+                    </tr>
+                    <?php } ?>
+                </tbody>
+            </table>
         </div>
     </div>
 
-    <div class="m-2">
+    <div class="mt-5">
+        <h4 class="text-primary">Detalle de Compra</h4>
         <table class="table table-striped bg-success rounded" style="--bs-bg-opacity: .2;">
             <thead>
                 <tr>
-                    <th><b>MATERIA PRIMA</b></th>
+                    <th><b>ID_MATERIA PRIMA</b></th>
                     <th><b>CANTIDAD</b></th>
                     <th><b>COSTO $</b></th>
                 </tr>
             </thead>
+
             <tbody>
                 <tr>
                     <td>
-                        <p class="text-dark"><b>ej m. prima</b></p>
+                        <!-- columna: MATERIA PRIMA -->
+                        <p class="text-dark"><b>1</b></p>
                     </td>
                     <td>
-                        <p class="text-dark"><b>2</b></p>
+                        <!-- columna: CANTIDAD -->
+                        <p class="text-dark"><b></b>12</p>
                     </td>
                     <td>
-                        <p class="text-dark"><b>200</b></p>
+                        <!-- columna: COSTO -->
+                        <p class="text-dark"><b>$3445</b></p>
                     </td>
                 </tr>
             </tbody>
--- listado_stock_critico.php
+++ listado_stock_critico.php
@@ -1,37 +1,37 @@
 <?php include("db.php") ?>
 <?php include("header.php") ?>
 
-<hr class="border border-danger">
-<h2 align="center" class="text-danger">LISTA STOCK CRÍTICO<i class="text-warning fas fa-exclamation-triangle m-3"></i></h2>
-<hr class="border border-danger">
 
-<div class="container text-center">
-    <div class="row my-5">
+<hr class="border border-danger">
+    <h3 class="text-danger text-center">LISTADO DE MATERIAS PRIMAS CON STOCK CRÍTICO<i class="text-warning fas fa-exclamation-triangle m-3"></i></h3>
+    <hr class="border border-danger">
 
-        <table class="table table-striped border-danger bg-danger" style="--bs-bg-opacity: .2;">
-            <thead>
-                <tr>
-                    <th>MATERIA PRIMA</th>
-                    <th>STOCK</th>
-                    <th>COSTO $</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>
-                        <p class="text-danger"><b>ej m. prima</b></p>
-                    </td>
-                    <td>
-                        <p class="text-danger"><b>ej stock</b></p>
-                    </td>
-                    <td>
-                        <p class="text-danger"><b>ej costo</b></p>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
+    <div class="container text-center">
+        <div class="row my-5">
+            <table class="table table-striped border-danger bg-danger" style="--bs-bg-opacity: .2;">
+                <thead>
+                    <tr>
+                        <th>ID</th>    
+                        <th>MATERIA PRIMA</th>
+                        <th>STOCK MÍNIMO</th>
+                        <th>STOCK ACTUAL</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                        $query = "SELECT * FROM materias_primas WHERE stock_actual<=stock_minimo ORDER BY id_materia_prima";
+                        $result = mysqli_query($conn, $query);    
+                        while($row = mysqli_fetch_assoc($result)) { ?>
+                        <tr>
+                            <td><?php echo $row['id_materia_prima']; ?></td>
+                            <td><?php echo $row['nombre_materia_prima']; ?></td>
+                            <td><?php echo $row['stock_minimo']; ?></td>
+                            <td><?php echo $row['stock_actual']; ?></td>
+                        </tr>
+                    <?php } ?>
+                </tbody>
+            </table>
+        </div>
     </div>
-</div>
-</div>
 
 <?php include("footer.php") ?>
